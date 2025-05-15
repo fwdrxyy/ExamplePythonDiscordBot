32c1eba16ef476456dba21462b98b7459eb61b31
Gabe <xyoxsp2yt@gmail.com>
2025-05-15 08:35:24 -0400

    Update example_commands.py

--- Slash Command Bot/Cogs/example_commands.py
+++ Slash Command Bot/Cogs/example_commands.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class General(commands.Cog):
+class example_commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -9,3 +9,6 @@ class General(commands.Cog):
     @discord.slash_command(name="ping", description="Sends the bot's latency.")    
     async def ping(self, ctx):
         await ctx.respond(f"Pong! Latency is {self.bot.latency * 1000:.2f}ms")
+
+def setup(bot):
+    bot.add_cog(example_commands(bot))
